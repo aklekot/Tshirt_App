bb96985c0646ddcb65b8d0fc834ec7726703a302
Agnieszka Klekot <74aklekot@gmail.com>
2024-12-18 12:12:23 +0100

    using useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import styles from '../Product/Product.module.scss';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm';
@@ -7,27 +7,35 @@ const Product = ({ name, title, basePrice, colors, sizes }) => {
   const [selectedColor, setSelectedColor] = useState(colors[0]);
   const [selectedSize, setSelectedSize] = useState(sizes[0]);
 
-  const getPrice = () => {
+ /* const getPrice = () => {
     return basePrice + selectedSize.additionalPrice;
+  };*/
+
+  const price = useMemo(() => {
+    return basePrice + selectedSize.additionalPrice;
+  }, [basePrice, selectedSize]); 
+
+ 
+  const handleSizeChange = (size) => {
+    setSelectedSize(size);
   };
 
   const imagePath = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${selectedColor}.jpg`;
 
   return (
     <article className={styles.product}>
-
       <ProductImage imagePath={imagePath} title={title} />
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductForm
           sizes={sizes}
           colors={colors}
           selectedSize={selectedSize}
           selectedColor={selectedColor}
-          onSizeChange={setSelectedSize}
+          onSizeChange={handleSizeChange}
           onColorChange={setSelectedColor}
         />
       </div>
